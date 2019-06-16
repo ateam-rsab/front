55ef9537ff3ab6e1964e25c61701ccf8d689747f
Salman Manoe <salmana6d@gmail.com>
2019-06-16 11:45:37 +0700

    Cabut validasi 1 tahun jatah cuti pegawai

--- app/javascripts/Setting.js
+++ app/javascripts/Setting.js
@@ -35,59 +35,60 @@ define([], function () {
         };
     } else {
         return {
-            BaseUrl: 'http://172.16.99.195:5555/app/data/GetRouting',
-            RouteUrl: 'http://172.16.99.195:5555/app/data/GetRouting',
-            UrlDataConfig: 'http://172.16.99.195:5555/app/data/GetRequireConfig',
-            baseUrlSerelize: "http://172.16.99.195:5555/app/data/Form/",
+            BaseUrl: 'http://localhost:5555/app/data/GetRouting',
+            RouteUrl: 'http://localhost:5555/app/data/GetRouting',
+            UrlDataConfig: 'http://localhost:5555/app/data/GetRequireConfig',
+            baseUrlSerelize: "http://localhost:5555/app/data/Form/",
 
             // URL BARU
             // K3KL
             baseUrlActionK3KL: "http://172.16.99.195:8080/jasamedika-k3kl/",
         	baseApiPostDataK3KL: "http://172.16.99.195:8080/jasamedika-k3kl/",
             /* SDM */
-            baseUrlActionSDM: 'http://172.16.99.195:8080/jasamedika-sdm/',
-            baseApiPostDataSDM: "http://172.16.99.195:8080/jasamedika-sdm/",
+            baseUrlActionSDM: 'http://localhost:8080/jasamedika-sdm/',
+            baseApiPostDataSDM: "http://localhost:8080/jasamedika-sdm/",
             /* Reporting */
-            urlReporting: 'http://192.168.12.2:8080/jasamedika-reporting/',
+            urlReporting: 'http://localhost:8080/jasamedika-reporting/',
             /* LAUNDRY */
             baseApiPostDataLaundry: "http://172.16.99.195:8080/jasamedika-laundry/",
             baseUrlActionLaundry: "http://172.16.99.195:8080/jasamedika-laundry/",
             /* IP3RS */
             baseUrlActionIPSRS: "http://172.16.99.195:8080/jasamedika-ipsrs/",
             baseApiPostDataIPSRS: "http://172.16.99.195:8080/jasamedika-ipsrs/",
-            urlSocket: 'http://172.16.99.195:5555',
+            urlSocket: 'http://localhost:5555',
 
-            urlRoute_SDM: 'http://172.16.99.195:5555/app/data/GetRoutingSDM',
-            urlPrinting: 'http://172.16.99.195:8080/jasamedika-web/',
-            baseUrlListData: "http://172.16.99.195:8080/jasamedika-web/service/list-generic/?view=",
-            baseApiUrlData: "http://172.16.99.195:8080/jasamedika-web/",
-            baseApiPostData: "http://172.16.99.195:8080/jasamedika-web/",
+            urlRoute_SDM: 'http://localhost:5555/app/data/GetRoutingSDM',
+            urlPrinting: 'http://localhost:8080/jasamedika-web/',
+            baseUrlListData: "http://localhost:8080/jasamedika-web/service/list-generic/?view=",
+            baseApiUrlData: "http://localhost:8080/jasamedika-web/",
+            baseApiPostData: "http://localhost:8080/jasamedika-web/",
             
 			// urlPrinting: 'https://smart.rsabhk.co.id:2222/jasamedika-web/',
 			// baseUrlListData: "https://smart.rsabhk.co.id:2222/jasamedika-web/service/list-generic/?view=",
 			// baseApiUrlData: "http://smart.rsabhk.co.id:2222/jasamedika-web/",
             // baseApiPostData: "http://smart.rsabhk.co.id:2222/jasamedika-web/",
 
-            baseUrlData: "http://172.16.99.195:5555/app/data/",
-            baseUrlAction: "http://172.16.99.195:8080/jasamedika-web/",
-            baseUrlLogin: "http://172.16.99.195:8080/jasamedika-web/auth/sign-in",
-            baseUrlLogout: "http://172.16.99.195:8080/jasamedika-web/auth/sign-out",
+            baseUrlData: "http://localhost:5555/app/data/",
+            baseUrlAction: "http://localhost:8080/jasamedika-web/",
+            baseUrlLogin: "http://localhost:8080/jasamedika-web/auth/sign-in",
+            baseUrlLogout: "http://localhost:8080/jasamedika-web/auth/sign-out",
 
 			// baseUrlAction: "https://smart.rsabhk.co.id:2222/jasamedika-web/",
 			// baseUrlLogin: "http://172.16.99.27:8000/service/auth/sign-in",
             // baseUrlLogout: "http://172.16.99.27:8000/service/auth/ssign-out",
 
             baseBridiging: "http://smart.rsabhk.co.id/Bridging/",
-            urlRoute: 'http://172.16.99.195:5555/app/data/GetRouting',
-            urlRoute_Akuntansi: 'http://172.16.99.195:5555/app/data/GetRoutingAkuntansi',
+            urlRoute: 'http://localhost:5555/app/data/GetRouting',
+            urlRoute_Akuntansi: 'http://localhost:5555/app/data/GetRoutingAkuntansi',
             
             //urlDataGeneric_Akuntansi: 'http://172.16.99.27:8000/service/list-generic?view=',
-			
-            urlDataGeneric_Akuntansi: 'http://192.168.12.3:2222/simrs_harkit/service/list-generic?view=',
-            urlDataTableMaster_Akuntansi: 'http://192.168.12.3:2222/simrs_harkit/service/master/',
-            urlDataTableTransaksi_Akuntansi: 'http://192.168.12.3:2222/simrs_harkit/service/transaksi/',
-            baseApiPostData_Akuntansi: 'http://192.168.12.3:2222/simrs_harkit/service/',
-            urlDataMaster: 'http://172.16.99.195:8080/jasamedika-web/generated_master/',
+            
+            urlDataGeneric_Akuntansi: 'http://smart.rsabhk.co.id/simrs_harkit/service/list-generic?view=',
+            urlDataTableMaster_Akuntansi: 'http://smart.rsabhk.co.id/simrs_harkit/service/master/',
+            urlDataTableTransaksi_Akuntansi: 'http://smart.rsabhk.co.id/simrs_harkit/service/transaksi/',
+            baseApiPostData_Akuntansi: 'http://smart.rsabhk.co.id/simrs_harkit/service/',
+
+            urlDataMaster: 'http://localhost:8080/jasamedika-web/generated_master/',
 			
             // urlDataMaster: 'http://smart.rsabhk.co.id:2222/jasamedika-web/generated_master/',
             // rabbitMQHost: 'amqp://rsab:rsab@172.16.99.27'
