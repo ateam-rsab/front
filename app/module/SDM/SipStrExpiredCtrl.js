81503bb6691c21ad829f20cb8c34c0d646dd6151
hrefel <hrefel@gmail.com>
2019-10-23 18:55:36 +0700

    tambah filtering

--- app/module/SDM/SipStrExpiredCtrl.js
+++ app/module/SDM/SipStrExpiredCtrl.js
@@ -1,12 +1,12 @@
-define(['initialize'], function(initialize) {
-	'use strict';
-	initialize.controller('SipStrExpiredCtrl', ['$rootScope', '$scope', '$timeout', 'ModelItem','$state','ManageSdm', 'ManageSdmNew', 'FindSdm', 'DateHelper',
-		function($rootScope, $scope, $timeout, ModelItem, $state, ManageSdm, ManageSdmNew, FindSdm, dateHelper) {
+define(['initialize'], function (initialize) {
+    'use strict';
+    initialize.controller('SipStrExpiredCtrl', ['$rootScope', '$scope', '$timeout', 'ModelItem', '$state', 'ManageSdm', 'ManageSdmNew', 'FindSdm', 'DateHelper',
+        function ($rootScope, $scope, $timeout, ModelItem, $state, ManageSdm, ManageSdmNew, FindSdm, dateHelper) {
             $scope.item = {};
-            $scope.yearSelected = { 
+            $scope.yearSelected = {
                 format: "MMMM yyyy",
-                start: "year", 
-                depth: "year" 
+                start: "year",
+                depth: "year"
             };
             function getSixMonths() {
                 let now = new Date();
@@ -24,9 +24,9 @@ define(['initialize'], function(initialize) {
                 return today;
             }
             $scope.batasData = dateHelper.getTanggalFormatted(new Date(new Date().getFullYear(), new Date().getMonth() + 6, getSixMonths()));
-            $scope.loadDataSip = function(){
+            $scope.loadDataSip = function () {
                 $scope.isRouteLoading = true;
-                ManageSdmNew.getListData("pegawai/get-pegawai-sip-expired").then(function(res){
+                ManageSdmNew.getListData("pegawai/get-pegawai-sip-expired").then(function (res) {
                     $scope.datagridSip = new kendo.data.DataSource({
                         data: res.data.data,
                         pageSize: 15,
@@ -37,7 +37,7 @@ define(['initialize'], function(initialize) {
                         //         aggregate: "count"
                         //     }]
                         // },
-                        aggregate: [ { field: "tglBerakhirSip2", aggregate: "count" }]
+                        aggregate: [{ field: "tglBerakhirSip2", aggregate: "count" }]
                     });
                     $scope.isRouteLoading = false;
                 }, (err) => {
@@ -45,9 +45,9 @@ define(['initialize'], function(initialize) {
                 });
                 $scope.tabActive = 'masaBerlakuSip';
             };
-            $scope.loadDataStr = function(){
+            $scope.loadDataStr = function () {
                 $scope.isRouteLoading = true;
-                ManageSdmNew.getListData("pegawai/get-pegawai-str-expired").then(function(res){
+                ManageSdmNew.getListData("pegawai/get-pegawai-str-expired").then(function (res) {
                     $scope.datagridStr = new kendo.data.DataSource({
                         data: res.data.data,
                         pageSize: 10,
@@ -58,7 +58,7 @@ define(['initialize'], function(initialize) {
                         //         aggregate: "count"
                         //     }]
                         // },
-                        aggregate: [ { field: "tglBerakhirStr2", aggregate: "count" }]
+                        aggregate: [{ field: "tglBerakhirStr2", aggregate: "count" }]
                     });
                     $scope.isRouteLoading = false;
                 }, (error) => {
@@ -67,13 +67,13 @@ define(['initialize'], function(initialize) {
                 $scope.tabActive = 'masaBerlakuStr';
             };
 
-            $scope.onTabChanges = function(value){
-                if (value === 1){
-                    if(!$scope.datagridSip){
+            $scope.onTabChanges = function (value) {
+                if (value === 1) {
+                    if (!$scope.datagridSip) {
                         $scope.loadDataSip();
                     }
-                } else if (value === 2){
-                    if(!$scope.datagridStr){
+                } else if (value === 2) {
+                    if (!$scope.datagridStr) {
                         $scope.loadDataStr();
                     }
                 }
@@ -81,67 +81,80 @@ define(['initialize'], function(initialize) {
 
             $scope.opsiGridSip = {
                 toolbar: [
-                // "excel", 
-                    { text: "export", name:"Export detail", template: '<button ng-click="exportDetailSIP()" class="k-button k-button-icontext k-grid-upload"><span class="k-icon k-i-excel"></span>Export to Excel</button>'}
+                    // "excel", 
+                    { text: "export", name: "Export detail", template: '<button ng-click="exportDetailSIP()" class="k-button k-button-icontext k-grid-upload"><span class="k-icon k-i-excel"></span>Export to Excel</button>' }
                 ],
+                filterable: {
+                    extra: false,
+                    operators: {
+                        string: {
+                            startswith: "Dimulai dengan",
+                            contains: "mengandung kata",
+                            neq: "Tidak mengandung kata"
+                        }
+                    }
+                },
                 excel: {
                     fileName: "Daftar SIP Pegawai.xlsx",
                     allPages: true,
                 },
-                excelExport: function(e){
+                excelExport: function (e) {
                     var sheet = e.workbook.sheets[0];
                     sheet.frozenRows = 2;
                     sheet.mergedCells = ["A1:F1"];
                     sheet.name = "Orders";
 
                     var myHeaders = [{
-                        value:"Daftar SIP Pegawai",
+                        value: "Daftar SIP Pegawai",
                         fontSize: 20,
                         textAlign: "center",
-                        background:"#ffffff",
-                         // color:"#ffffff"
-                     }];
+                        background: "#ffffff",
+                        // color:"#ffffff"
+                    }];
 
-                     sheet.rows.splice(0, 0, { cells: myHeaders, type: "header", height: 70});
-                 },
+                    sheet.rows.splice(0, 0, { cells: myHeaders, type: "header", height: 70 });
+                },
                 pageable: true,
                 selectable: "row",
                 scrollable: false,
                 columns: [
-                    { field: "nipPns", title: "<h3>NIP</h3>", "width": 180}, 
-                    { field: "namaLengkap", title: "<h3>Nama</h3>", "width": 280}, 
+                    {
+                        field: "nipPns", title: "<h3>NIP</h3>", "width": 180
+                    },
+                    { field: "namaLengkap", title: "<h3>Nama</h3>", "width": 280 },
                     { field: "unitKerja", title: "<h3>Unit Kerja</h3>" },
                     { field: "subUnitKerja", title: "<h3>Sub-Unit Kerja</h3>" },
                     { field: "noSip", title: "<h3>Nomor SIP</h3>" },
-                    { field: "tglBerakhirSip2", title: "<h3>Tanggal Berakhir</h3>"
+                    {
+                        field: "tglBerakhirSip2", title: "<h3>Tanggal Berakhir</h3>"
                         // aggregates: ["count"],
                         // groupHeaderTemplate: "Tanggal Berakhir SIP [#= kendo.toString(value) #] (Total: #= count#)" 
                     }
-                 
-               ]
+
+                ]
             };
 
-            $scope.exportDetailSIP = function(){
+            $scope.exportDetailSIP = function () {
                 var tempDataExport = [];
                 var rows = [
-                        {
-                            cells: [
-                                { value: "NIP" },
-                                { value: "Nama" },
-                                { value: "Unit Kerja" },
-                                { value: "Sub Unit Kerja" },
-                                { value: "No. SIP" },
-                                { value: "Tanggal Berakhir" }
-                                
-                            ]
-                        }
-                    ];
-                
+                    {
+                        cells: [
+                            { value: "NIP" },
+                            { value: "Nama" },
+                            { value: "Unit Kerja" },
+                            { value: "Sub Unit Kerja" },
+                            { value: "No. SIP" },
+                            { value: "Tanggal Berakhir" }
+
+                        ]
+                    }
+                ];
+
                 tempDataExport = $scope.datagridSip;
-                tempDataExport.fetch(function(){
+                tempDataExport.fetch(function () {
                     var data = this.data();
                     console.log(data);
-                    for (var i = 0; i < data.length; i++){
+                    for (var i = 0; i < data.length; i++) {
                         //push single row for every record
                         rows.push({
                             cells: [
@@ -176,70 +189,71 @@ define(['initialize'], function(initialize) {
                         ]
                     });
                     //save the file as Excel file with extension xlsx
-                    kendo.saveAs({dataURI: workbook.toDataURL(), fileName: "Daftar Masa Berlaku SIP-"+ dateHelper.formatDate(new Date(), 'DD-MMM-YYYY') +".xlsx"});
+                    kendo.saveAs({ dataURI: workbook.toDataURL(), fileName: "Daftar Masa Berlaku SIP-" + dateHelper.formatDate(new Date(), 'DD-MMM-YYYY') + ".xlsx" });
                 });
             };
 
             $scope.opsiGridStr = {
                 toolbar: [
-                    { text: "export", name:"Export detail", template: '<button ng-click="exportDetailSTR()" class="k-button k-button-icontext k-grid-upload"><span class="k-icon k-i-excel"></span>Export to Excel</button>'},
+                    { text: "export", name: "Export detail", template: '<button ng-click="exportDetailSTR()" class="k-button k-button-icontext k-grid-upload"><span class="k-icon k-i-excel"></span>Export to Excel</button>' },
                 ],
                 excel: {
                     fileName: "Daftar STR Pegawai.xlsx",
                     allPages: true,
                 },
-                excelExport: function(e){
+                excelExport: function (e) {
                     var sheet = e.workbook.sheets[0];
                     sheet.frozenRows = 2;
                     sheet.mergedCells = ["A1:G1"];
                     sheet.name = "Orders";
 
                     var myHeaders = [{
-                        value:"Daftar STR Pegawai",
+                        value: "Daftar STR Pegawai",
                         fontSize: 20,
                         textAlign: "center",
-                        background:"#ffffff",
-                         // color:"#ffffff"
-                     }];
+                        background: "#ffffff",
+                        // color:"#ffffff"
+                    }];
 
-                     sheet.rows.splice(0, 0, { cells: myHeaders, type: "header", height: 70});
-                 },
+                    sheet.rows.splice(0, 0, { cells: myHeaders, type: "header", height: 70 });
+                },
                 pageable: true,
                 selectable: "row",
                 scrollable: false,
                 columns: [
-                    { field: "nipPns", title: "<h3>NIP</h3>", "width": 180}, 
-                    { field: "namaLengkap", title: "<h3>Nama</h3>", "width": 280}, 
+                    { field: "nipPns", title: "<h3>NIP</h3>", "width": 180 },
+                    { field: "namaLengkap", title: "<h3>Nama</h3>", "width": 280 },
                     { field: "unitKerja", title: "<h3>Unit Kerja</h3>" },
                     { field: "subUnitKerja", title: "<h3>Sub-Unit Kerja</h3>" },
                     { field: "noStr", title: "<h3>Nomor STR</h3>" },
-                    { field: "tglBerakhirStr2", title: "<h3>Tanggal Berakhir</h3>"
-                    // aggregates: ["count"], groupHeaderTemplate: "Tanggal Berakhir STR [#= kendo.toString(value) #] (Total: #= count#)"
-                  }
-               ]
+                    {
+                        field: "tglBerakhirStr2", title: "<h3>Tanggal Berakhir</h3>"
+                        // aggregates: ["count"], groupHeaderTemplate: "Tanggal Berakhir STR [#= kendo.toString(value) #] (Total: #= count#)"
+                    }
+                ]
             };
 
-            $scope.exportDetailSTR = function(){
+            $scope.exportDetailSTR = function () {
                 var tempDataExport = [];
                 var rows = [
-                        {
-                            cells: [
-                                { value: "NIP" },
-                                { value: "Nama" },
-                                { value: "Unit Kerja" },
-                                { value: "Sub Unit Kerja" },
-                                { value: "No. STR" },
-                                { value: "Tanggal Berakhir" }
-                                
-                            ]
-                        }
-                    ];
-                
+                    {
+                        cells: [
+                            { value: "NIP" },
+                            { value: "Nama" },
+                            { value: "Unit Kerja" },
+                            { value: "Sub Unit Kerja" },
+                            { value: "No. STR" },
+                            { value: "Tanggal Berakhir" }
+
+                        ]
+                    }
+                ];
+
                 tempDataExport = $scope.datagridStr;
-                tempDataExport.fetch(function(){
+                tempDataExport.fetch(function () {
                     var data = this.data();
                     console.log(data);
-                    for (var i = 0; i < data.length; i++){
+                    for (var i = 0; i < data.length; i++) {
                         //push single row for every record
                         rows.push({
                             cells: [
@@ -274,48 +288,48 @@ define(['initialize'], function(initialize) {
                         ]
                     });
                     //save the file as Excel file with extension xlsx
-                    kendo.saveAs({dataURI: workbook.toDataURL(), fileName: "Daftar Masa Berlaku STR-"+ dateHelper.formatDate(new Date(), 'DD-MMM-YYYY') +".xlsx"});
+                    kendo.saveAs({ dataURI: workbook.toDataURL(), fileName: "Daftar Masa Berlaku STR-" + dateHelper.formatDate(new Date(), 'DD-MMM-YYYY') + ".xlsx" });
                 });
             };
 
             var timeoutPromise;
-            $scope.$watch('item.tglAwalSip', function(newVal, oldVal){
+            $scope.$watch('item.tglAwalSip', function (newVal, oldVal) {
                 $timeout.cancel(timeoutPromise);
-                timeoutPromise = $timeout(function(){
-                    if (newVal && newVal !== oldVal){
+                timeoutPromise = $timeout(function () {
+                    if (newVal && newVal !== oldVal) {
                         applyFilterDate("#gridSip", "tglBerakhirSip", "gte", newVal)
                     }
                 }, 500);
             });
-            $scope.$watch('item.tglAkhirSip', function(newVal, oldVal){
+            $scope.$watch('item.tglAkhirSip', function (newVal, oldVal) {
                 $timeout.cancel(timeoutPromise);
-                timeoutPromise = $timeout(function(){
-                    if (newVal && newVal !== oldVal){
+                timeoutPromise = $timeout(function () {
+                    if (newVal && newVal !== oldVal) {
                         applyFilterDate("#gridSip", "tglBerakhirSip", "lte", newVal)
                     }
                 }, 500);
             });
-            $scope.$watch('item.tglAwalStr', function(newVal, oldVal){
+            $scope.$watch('item.tglAwalStr', function (newVal, oldVal) {
                 $timeout.cancel(timeoutPromise);
-                timeoutPromise = $timeout(function(){
-                    if (newVal && newVal !== oldVal){
+                timeoutPromise = $timeout(function () {
+                    if (newVal && newVal !== oldVal) {
                         applyFilterDate("#gridStr", "tglBerakhirStr", "gte", newVal)
                     }
                 }, 500);
             });
-            $scope.$watch('item.tglAkhirStr', function(newVal, oldVal){
+            $scope.$watch('item.tglAkhirStr', function (newVal, oldVal) {
                 $timeout.cancel(timeoutPromise);
-                timeoutPromise = $timeout(function(){
-                    if (newVal && newVal !== oldVal){
+                timeoutPromise = $timeout(function () {
+                    if (newVal && newVal !== oldVal) {
                         applyFilterDate("#gridStr", "tglBerakhirStr", "lte", newVal)
                     }
                 }, 500);
             });
-            function applyFilterDate(gridId, filterField, filterOperator, filterValue){
+            function applyFilterDate(gridId, filterField, filterOperator, filterValue) {
                 var gridData = $(gridId).data("kendoGrid");
                 var currFilterObj = gridData.dataSource.filter();
                 var currentFilters = currFilterObj ? currFilterObj.filters : [];
-                
+
                 if (currentFilters && currentFilters.length > 0) {
                     for (var i = 0; i < currentFilters.length; i++) {
                         if (currentFilters[i].field == filterField && currentFilters[i].operator == filterOperator) {
@@ -327,9 +341,9 @@ define(['initialize'], function(initialize) {
 
                 if (filterValue !== "") {
                     var tgl;
-                    if(filterOperator === "gte") {
+                    if (filterOperator === "gte") {
                         tgl = dateHelper.setJamAwal(new Date(filterValue));
-                    } else if(filterOperator === "lte"){
+                    } else if (filterOperator === "lte") {
                         tgl = dateHelper.setJamAkhir(new Date(filterValue));
                     }
                     currentFilters.push({
@@ -344,11 +358,11 @@ define(['initialize'], function(initialize) {
                     filters: currentFilters
                 });
             };
-            $scope.resetFilter = function(gridId){
+            $scope.resetFilter = function (gridId) {
                 var gridData = $(gridId).data("kendoGrid");
                 gridData.dataSource.filter({});
                 $scope.item = {};
             };
-		}
-	]);
+        }
+    ]);
 });
