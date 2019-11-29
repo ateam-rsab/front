37b8603304c15cc64e49a84f4afbbf754a2ac543
salmanoe <salmanoe@live.com>
2019-11-29 14:39:00 +0700

    menu rekam data program pendidikan dokter spesialis

--- app/module/SDM/DataPesertaDidikCtrl.js
+++ app/module/SDM/DataPesertaDidikCtrl.js
@@ -69,7 +69,7 @@ define(['initialize'], function (initialize) {
                 // };
                 // $scope.ListKedudukanPegawai = result[5].data.data;
                 if (result[1].statResponse) {
-                    var toRemove = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22,24,25,27,28],
+                    var toRemove = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,22,25,27,28],
                         listKedudukan = result[1].data.data;
 
                     $scope.ListKedudukanPegawai = listKedudukan.filter(function(el) {
