6585459d310d6de6a70c0dd6c8a9478bf7da1ceb
hrefel <hrefel@gmail.com>
2020-04-28 12:57:10 +0700

    done implement real time date

--- app/module/SDM/PresensiPegawaiCtrl.js
+++ app/module/SDM/PresensiPegawaiCtrl.js
@@ -61,8 +61,8 @@ define(['initialize'], function (initialize) {
 
             $scope.savePresensi = function () {
                 let data = {
-                    tr_date: DateHelper.toTimeStamp(new Date()),
-                    tr_time: DateHelper.toTimeStamp(new Date()),
+                    // tr_date: DateHelper.toTimeStamp(new Date()),
+                    // tr_time: DateHelper.toTimeStamp(new Date()),
                     empl_code: $scope.data.idFinger,
                     processtatus: $scope.isWFH ? 1 : 0
                 }
