ca502382a7dd84f0dbd98560f71b1dbb59a62905
Salman Nagabonar <salmanoe@live.com>
2020-04-29 10:50:39 +0700

    perbaikan service simpan loginuser

--- app/module/MasterData/UbahPasswordCtrl.js
+++ app/module/MasterData/UbahPasswordCtrl.js
@@ -1,37 +1,37 @@
-define(['initialize'], function(initialize) {
+define(['initialize'], function (initialize) {
 	'use strict';
-	initialize.controller('UbahPasswordCtrl', ['$q', '$rootScope', '$scope', '$window', 'ManageSarprasPhp', 'ModelItem','$http','ManageSarpras',
-		function($q, $rootScope, $scope, $window, manageSarprasPhp, modelItem, $http,manageSarpras) {
+	initialize.controller('UbahPasswordCtrl', ['$q', '$rootScope', '$scope', '$window', 'ManageSarprasPhp', 'ModelItem', '$http', 'ManageSarpras',
+		function ($q, $rootScope, $scope, $window, manageSarprasPhp, modelItem, $http, manageSarpras) {
 			$scope.item = {};
 			$scope.dataVOloaded = true;
-			$scope.now = new Date();	
+			$scope.now = new Date();
 			init();
-		    $scope.pegawai = modelItem.getPegawai();
-		 	$scope.datauserlogin = JSON.parse(window.localStorage.getItem("datauserlogin"));
-			$scope.item.namaPegawai= $scope.pegawai.namaLengkap;
+			$scope.pegawai = modelItem.getPegawai();
+			$scope.datauserlogin = JSON.parse(window.localStorage.getItem("datauserlogin"));
+			$scope.item.namaPegawai = $scope.pegawai.namaLengkap;
 
-		    getUserLoginData();
-			function getUserLoginData (){
-			 	 manageSarprasPhp.getDataTableTransaksi("pegawai/svc-modul?get=loginuser&id="+$scope.datauserlogin .id).then(function(data){
+			getUserLoginData();
+			function getUserLoginData() {
+				manageSarprasPhp.getDataTableTransaksi("pegawai/svc-modul?get=loginuser&id=" + $scope.datauserlogin.id).then(function (data) {
 					$scope.item.idlogin = data.data.loginuser[0].luid;
 					$scope.item.namaUser = data.data.loginuser[0].namauser;
 					// $scope.item.kataKunciPass = data.data.loginuser.katasandi;
 					// $scope.item.kataKunciConfirm = data.data.loginuser.katasandi;
-					$scope.item.kelompokUserHakAkses ={id:data.data.loginuser[0].kuid,kelompokuser:data.data.loginuser[0].kelompokuser} ;
+					$scope.item.kelompokUserHakAkses = { id: data.data.loginuser[0].kuid, kelompokuser: data.data.loginuser[0].kelompokuser };
 					$scope.dataGrid = data.data.loginuser[0].data;
-					
-					
+
+
 				})
 			}
 			init();
-			
+
 			function init() {
-				manageSarprasPhp.getDataTableTransaksi("pegawai/svc-modul?get=kelompokuser").then(function(datapegawai){
+				manageSarprasPhp.getDataTableTransaksi("pegawai/svc-modul?get=kelompokuser").then(function (datapegawai) {
 					$scope.listKelompokuser = datapegawai.data;
 				});
 			}
 
-			$scope.simpan = function(){
+			$scope.simpan = function () {
 				if ($scope.item.idlogin == undefined) {
 					alert('Pilih dahullu pegawai!')
 					return
@@ -40,33 +40,38 @@ define(['initialize'], function(initialize) {
 					alert('Kata kunci tidak sama')
 					return
 				}
-			
-                var objSave = {
-                        id:$scope.item.idlogin,
-						kelompokUser:{  
-					      id:$scope.item.kelompokUserHakAkses.id
-						},
-						statusLogin:0,
-						kataSandi:$scope.item.kataKunciPass,
-						namaUser:$scope.item.namaUser,
-						pegawai:{  
-					      id:$scope.pegawai.id
-						}
-                    }
-                
-                manageSarprasPhp.saveDataTransaksi('admin/ubah-password',objSave).then(function(e) {
-                // manageSarpras.saveLoginUser(objSave).then(function(e) {
 
-                })
+				var objSave = {
+					id: $scope.item.idlogin,
+					kelompokUser: {
+						id: $scope.item.kelompokUserHakAkses.id
+					},
+					statusLogin: 0,
+					kataSandi: $scope.item.kataKunciPass,
+					namaUser: $scope.item.namaUser,
+					pegawai: {
+						id: $scope.pegawai.id
+					}
+				}
+
+				//save to php passcode column
+				manageSarprasPhp.saveDataTransaksi('admin/ubah-password', objSave).then(function (e) {
+
+				})
+
+				//save to java katasandi column
+				manageSarpras.saveLoginUser(objSave).then(function (e) {
+
+				})
 			}
 
-			$scope.batal2=function(){
+			$scope.batal2 = function () {
 				window.history.back();
 			}
 
-		
+
 
 		}
-		]
-		);
+	]
+	);
 });
