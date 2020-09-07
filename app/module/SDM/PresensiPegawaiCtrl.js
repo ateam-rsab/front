55c05d29fb478a35cd1a651a3390b99cd580063d
salmanoe <salmanoe@live.com>
2020-09-07 15:36:58 +0700

    penambahan get latitude longitude

--- app/module/SDM/PresensiPegawaiCtrl.js
+++ app/module/SDM/PresensiPegawaiCtrl.js
@@ -38,10 +38,6 @@ define(['initialize'], function (initialize) {
                     $('#time-part').html(momentNow.format('HH:mm:ss'));
                 }, 100);
 
-                $.get('http://www.geoplugin.net/json.gp', function (data) {
-                    $scope.dataDevice = JSON.parse(data);
-                });
-
                 $scope.tanggalPresensi = new Date();
                 getDataHistory();
                 ManageSdmNew.getListData('sdm/get-jadwal-pegawai?idPegawai=' + $scope.dataPegawaiLogin.id).then((res) => {
@@ -54,10 +50,10 @@ define(['initialize'], function (initialize) {
             init();
 
             $scope.columnGrid = [{
-                    "field": "jam",
-                    "title": "<h3>Jam</h3>",
-                    "width": "150px"
-                },
+                "field": "jam",
+                "title": "<h3>Jam</h3>",
+                "width": "150px"
+            },
 
             ];
 
@@ -68,6 +64,12 @@ define(['initialize'], function (initialize) {
             }
 
             $scope.savePresensi = function () {
+                $.get('http://www.geoplugin.net/json.gp', function (data) {
+                    $scope.dataDevice = JSON.parse(data);
+                });
+
+                getLocation();
+
                 let data = {
                     // tr_date: DateHelper.toTimeStamp(new Date()),
                     // tr_time: DateHelper.toTimeStamp(new Date()),
@@ -76,7 +78,10 @@ define(['initialize'], function (initialize) {
                     },
                     // empl_code: $scope.data.idFinger,
                     processtatus: $scope.data.isWFH ? 1 : 0,
-                    ip_addr: $scope.dataDevice.geoplugin_request
+                    ip_addr: $scope.dataDevice.geoplugin_request,
+                    latitude: $scope.latitude,
+                    longitude: $scope.longitude,
+                    akurasi: $scope.akurasi
                 }
                 // console.log(data);
                 ManageSdmNew.saveData(data, 'sdm/save-presensi-pegawai/').then((res) => {
@@ -84,6 +89,46 @@ define(['initialize'], function (initialize) {
                 })
             }
 
+            var x = document.getElementById("map");
+            function getLocation() {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(showPosition);
+                } else {
+                    x.innerHTML = "Geolocation is not supported by this browser.";
+                }
+            }
+
+            function showPosition(position) {
+                $scope.latitude = position.coords.latitude;
+                $scope.longitude = position.coords.longitude;
+                $scope.akurasi = position.coords.accuracy;
+                var latMin = Math.floor(getDecimal(Math.abs(position.coords.latitude)) * 60);
+                var latSec = getDecimal(getDecimal(Math.abs(position.coords.latitude)) * 60) * 60;
+                var longMin = Math.floor(getDecimal(Math.abs(position.coords.longitude)) * 60);
+                var longSec = getDecimal(getDecimal(Math.abs(position.coords.longitude)) * 60) * 60;
+
+                if (position.coords.latitude < 0 && position.coords.longitude < 0) {
+                    x.innerHTML = "Lintang: " + Math.floor(Math.abs(position.coords.latitude)) + "&#176;" + latMin + "&#8242;" + latSec.toFixed(2) + "&#8243;LS" +
+                        ", Bujur: " + Math.floor(Math.abs(position.coords.longitude)) + "&#176;" + longMin + "&#8242;" + longSec.toFixed(2) + "&#8243;BB" +
+                        ", Akurasi: " + position.coords.accuracy + " meter";
+                } else if (position.coords.latitude > 0 && position.coords.longitude > 0) {
+                    x.innerHTML = "Lintang: " + Math.floor(Math.abs(position.coords.latitude)) + "&#176;" + latMin + "&#8242;" + latSec.toFixed(2) + "&#8243;LU" +
+                        ", Bujur: " + Math.floor(Math.abs(position.coords.longitude)) + "&#176;" + longMin + "&#8242;" + longSec.toFixed(2) + "&#8243;BT" +
+                        ", Akurasi: " + position.coords.accuracy + " meter";
+                } else if (position.coords.latitude < 0 && position.coords.longitude > 0) {
+                    x.innerHTML = "Lintang: " + Math.floor(Math.abs(position.coords.latitude)) + "&#176;" + latMin + "&#8242;" + latSec.toFixed(2) + "&#8243;LS" +
+                        ", Bujur: " + Math.floor(Math.abs(position.coords.longitude)) + "&#176;" + longMin + "&#8242;" + longSec.toFixed(2) + "&#8243;BT" +
+                        ", Akurasi: " + position.coords.accuracy + " meter";
+                } else if (position.coords.latitude > 0 && position.coords.longitude < 0) {
+                    x.innerHTML = "Lintang: " + Math.floor(Math.abs(position.coords.latitude)) + "&#176;" + latMin + "&#8242;" + latSec.toFixed(2) + "&#8243;LU" +
+                        ", Bujur: " + Math.floor(Math.abs(position.coords.longitude)) + "&#176;" + longMin + "&#8242;" + longSec.toFixed(2) + "&#8243;BB" +
+                        ", Akurasi: " + position.coords.accuracy + " meter";
+                }
+            }
+
+            function getDecimal(n) {
+                return (n - Math.floor(n));
+            }
 
         }
     ]);
