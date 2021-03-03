bd648dc2045519fcbc93a38f583869fdf16bfab5
hrefel <hrefel@gmail.com>
2021-03-03 09:40:02 +0700

    done batal pasien reservasi dan fixing date format kontrak kinerja individu

--- app/module/Laboratorium/DashboardAntrianPasienLabCtrl.js
+++ app/module/Laboratorium/DashboardAntrianPasienLabCtrl.js
@@ -3,18 +3,27 @@ define(['initialize'], function (initialize) {
     initialize.controller('DashboardAntrianPasienLabCtrl', ['$q', '$rootScope', '$scope', 'ManageLogistikPhp', '$state', 'CacheHelper', 'DateHelper',
         function ($q, $rootScope, $scope, manageLogistikPhp, $state, cacheHelper, dateHelper) {
             let intervalTime = 5 * 1000;
+            let paramTgl = {};
+            let tglAwal = new Date();
+            let tglAkhir = new Date();
             $scope.dataPasienAntrian = [];
 
             let init = () => {
-                setInterval(() => {
-                    init();
-                }, intervalTime)
-                manageLogistikPhp.getDataTableTransaksi("lab-radiologi/get-daftar-order-lab?isNotVerif=true&jmlRow=10").then(res => {
-                    $scope.dataPasienAntrian = res.data.data
+                tglAwal = dateHelper.formatDate(tglAwal.setHours(0, 0, 0, 0), "YYYY-MM-DD HH:mm:ss");
+                tglAkhir = dateHelper.formatDate(tglAkhir.setHours(23, 59, 0, 0), "YYYY-MM-DD HH:mm:ss");
+
+                manageLogistikPhp.getDataTableTransaksi("lab-radiologi/get-dashboard?tglAwal=" + tglAwal + "&tglAkhir=" + tglAkhir).then(res => { 
+                    console.log(res);
                 })
+                // setInterval(() => {
+                //     init();
+                // }, intervalTime)
+                // manageLogistikPhp.getDataTableTransaksi("lab-radiologi/get-daftar-order-lab?isNotVerif=true&jmlRow=10").then(res => {
+                //     $scope.dataPasienAntrian = res.data.data
+                // })
             }
             init();
-            
+
         }
     ]);
 });
