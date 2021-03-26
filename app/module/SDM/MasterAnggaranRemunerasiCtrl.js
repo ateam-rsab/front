f730c5d8457190c72bca9ac976c6c0ca27e051be
hrefel <hrefel@gmail.com>
2021-03-26 22:33:45 +0700

    add verifikasi catatan kegiatan harian dan save all master anggaran remun

--- app/module/SDM/MasterAnggaranRemunerasiCtrl.js
+++ app/module/SDM/MasterAnggaranRemunerasiCtrl.js
@@ -1,13 +1,14 @@
 define(['initialize'], function (initialize) {
     'use strict';
-    initialize.controller('MasterAnggaranRemunerasiCtrl', ['$q', '$rootScope', '$scope', 'ModelItem', '$state', 'NamaAsuransi', 'ManageSdm', 'ManageSdmNew', '$timeout',
-        function ($q, $rootScope, $scope, ModelItem, $state, NamaAsuransi, ManageSdm, ManageSdmNew, $timeout) {
+    initialize.controller('MasterAnggaranRemunerasiCtrl', ['$q', '$rootScope', '$scope', 'ModelItem', '$state', 'NamaAsuransi', 'ManageSdm', 'ManageSdmNew', '$timeout', "DateHelper",
+        function ($q, $rootScope, $scope, ModelItem, $state, NamaAsuransi, ManageSdm, ManageSdmNew, $timeout, DateHelper) {
             $scope.item = {};
             $scope.edit = {};
             $scope.totalNilaiJabatan = 0;
             $scope.item.anggaranRemunerasi = 0;
             $scope.item.pir = $scope.item.anggaranRemunerasi + "/" + 13;
             $scope.item.pirHitungan = 0;
+            let dataLogin = JSON.parse(localStorage.getItem("datauserlogin"));
 
             $scope.optGridPenempatanEvaluasiJabatan = {
                 pageable: true,
@@ -139,13 +140,15 @@ define(['initialize'], function (initialize) {
                 });
 
                 $scope.getDataPlafon();
-               
+
             }
             $scope.init();
 
             $scope.changeAnggaran = () => {
                 $scope.item.pir = Math.ceil($scope.item.anggaranRemunerasi / 13);
                 $scope.item.pirHitungan = Math.ceil($scope.item.pir / $scope.totalNilaiJabatan);
+                $scope.item.pirUnformatted = $scope.item.pir;
+                $scope.item.pirHitunganUnformatted = $scope.item.pirHitungan;
 
                 let pirFormatted = $scope.item.pir.toLocaleString('id-ID', {
                         style: 'currency',
@@ -166,19 +169,46 @@ define(['initialize'], function (initialize) {
             }
 
             $scope.simpanEdit = () => {
-                // http://192.168.12.3:8080/jasamedika-sdm/sdm/save-plafon-remunerasi
+
                 let data = {
-                    grade:$scope.edit.grade,
-                    gajiHonorarium:$scope.edit.gaji,
-                    minInsentif:$scope.edit.minInsentif,
-                    maxInsentif:$scope.edit.maxInsentif,
-                    maxTotalRemunerasi:$scope.edit.totalRemunerasi,
+                    grade: $scope.edit.grade,
+                    gajiHonorarium: $scope.edit.gaji,
+                    minInsentif: $scope.edit.minInsentif,
+                    maxInsentif: $scope.edit.maxInsentif,
+                    maxTotalRemunerasi: $scope.edit.totalRemunerasi,
                 }
                 ManageSdmNew.saveData(data, "sdm/save-plafon-remunerasi").then(res => {
                     $scope.getDataPlafon();
                     $scope.closePopUp();
-                    
+
                 })
+            }
+
+            $scope.simpanAllAnggaran = () => {
+                let dataSource = $scope.dataPlafon._data;
+                console.log(dataSource);
+                let dataSave = []
+
+                for (let i = 0; i < dataSource.length; i++) {
+                    dataSave.push({
+                        tahun: DateHelper.toTimeStamp(new Date),
+                        totalNilaiJabatan: $scope.totalNilaiJabatan,
+                        anggaranRemunTahun: $scope.item.anggaranRemunerasi,
+                        anggaranRemunBulan: $scope.item.pirUnformatted,
+                        poinindeksrupiah: $scope.item.pirHitunganUnformatted,
+                        grade: {
+                            id: dataSource[i].grade
+                        },
+                        gajiHonorarium: dataSource[i].gajiHonorarium,
+                        minInsentif: dataSource[i].minInsentif,
+                        maxInsentif: dataSource[i].maxInsentif,
+                        maxTotalRemunerasi: dataSource[i].maxTotalRemunerasi,
+                        kdProfile: 0,
+                        statusEnabled: true
+                    }, )
+                }
+                // http://192.168.12.3:8080/jasamedika-sdm/iki-remunerasi/save-all-anggaran-remunerasi?loginUserId=29999
+                ManageSdmNew.saveData(dataSave, "iki-remunerasi/save-all-anggaran-remunerasi?loginUserId=" + dataLogin.id).then(res => { })
             }
         }
     ]);
