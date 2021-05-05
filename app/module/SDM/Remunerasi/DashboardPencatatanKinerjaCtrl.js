2fa2c435b37304f8e75e8220c0c0f986e30d5e53
salmanoe <salmanoe.rsabhk@gmail.com>
2021-05-05 09:19:59 +0700

    perbaikan filter bulan di dashboard pencatatan kinerja

--- app/module/SDM/Remunerasi/DashboardPencatatanKinerjaCtrl.js
+++ app/module/SDM/Remunerasi/DashboardPencatatanKinerjaCtrl.js
@@ -7,10 +7,9 @@ define(['initialize'], function (initialize) {
             $scope.showDetailPerilaku = false;
             $scope.showIsSingleJabatan = false;
             $scope.showSyaratKetentuan = false;
+
             $scope.item = {};
-            $scope.item.tanggal = new Date();
-            $scope.now = new Date();
-            $scope.disabledButtonAdd = true;
+
             $scope.dataLogin = JSON.parse(localStorage.getItem("pegawai"));
 
             var listKategoriRemun = [1, 10, 14]
@@ -21,10 +20,13 @@ define(['initialize'], function (initialize) {
             let getJabatanPegawai = () => {
                 ManageSdmNew.getListData("pegawai/get-all-jabatan-by-pegawai?idPegawai=" + $scope.dataLogin.id).then((res) => {
                     $scope.showIsSingleJabatan = res.data.data.length === 1;
+
                     $scope.dataSingleJabatan = res.data.data[0].namaJabatan;
                     $scope.idJabatanSingle = res.data.data[0].id;
 
                     $scope.listJabatan = res.data.data;
+
+                    $scope.item.bulan = new Date();
 
                     $scope.getDataDashboard();
                 })
@@ -32,23 +34,28 @@ define(['initialize'], function (initialize) {
 
             getJabatanPegawai();
 
+            $scope.monthSelectorOptions = {
+                start: "year",
+                depth: "year"
+            };
+
             $scope.getDataDashboard = () => {
-                let date = dateHelper.toTimeStamp($scope.item.tanggal);
-                let awalBulan = new Date();
-                awalBulan = new Date(awalBulan.getFullYear(), awalBulan.getMonth(), 1);
-                awalBulan = dateHelper.toTimeStamp(awalBulan);
-                console.log(awalBulan);
-                $scope.disabledButtonAdd = date < awalBulan;
-                console.log(date > awalBulan);
-                var jabatanId = "";
+                let bulan = new Date();
+                if ($scope.item.bulan) {
+                    bulan = $scope.item.bulan
+                }
+                let date = dateHelper.toTimeStamp(bulan);
+
+                var jabatanId = ""
                 if ($scope.showIsSingleJabatan) {
-                    jabatanId = $scope.idJabatanSingle;
+                    jabatanId = $scope.idJabatanSingle
                 } else {
                     jabatanId = ($scope.item.jabatan ? $scope.item.jabatan.id : "")
                 }
+
                 ManageSdmNew.getListData("iki-remunerasi/get-dashboard-kinerja?pegawaiId=" + $scope.dataLogin.id + "&jabatanId=" + jabatanId + "&bulan=" + date).then((res) => {
                     $scope.dataDashboard = res.data.data;
-                    console.log($scope.dataDashboard);
+                    // console.log($scope.dataDashboard);
 
                     $scope.dataDetailDashboardKinerja = {
                         kuantitas: res.data.data.listJenisIndikator[0],
@@ -59,7 +66,6 @@ define(['initialize'], function (initialize) {
                         persentaseCapaianPerilaku: res.data.data.listJenisIndikator[2].persenCapaianDibulatkan,
 
                     };
-
                     // console.log($scope.dataDetailDashboardKinerja);
                 });
             }
@@ -89,6 +95,7 @@ define(['initialize'], function (initialize) {
             $scope.addData = (data) => {
                 $scope.dataAdd = data;
                 $scope.labelData = data.namaIndikator;
+
                 $scope.popupAdd.open().center();
             }
 
