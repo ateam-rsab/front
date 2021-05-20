b3975c3bad98d83af97fd9a4f17cb99741ffc3a0
salmanoe <salmanoe.rsabhk@gmail.com>
2021-05-20 20:20:51 +0700

    - perbaikan hilangnya tombol simpan anggaran remunerasi jika sudah simpan untuk tahun berjalan - perbaikan isian numerik untuk target, bobot, dan entri hasil pada pencatatan kinerja harian

--- app/module/SDM/Remunerasi/DashboardPencatatanKinerjaCtrl.js
+++ app/module/SDM/Remunerasi/DashboardPencatatanKinerjaCtrl.js
@@ -263,6 +263,12 @@ define(['initialize'], function (initialize) {
                 $scope.resetForm();
                 $scope.popupAdd.close();
             }
+
+            document.querySelector(".numerik").addEventListener("keypress", function (evt) {
+                if (evt.which != 8 && evt.which != 0 && (evt.which > 31 && (evt.which != 46 && (evt.which < 48 || evt.which > 57)))) {
+                    evt.preventDefault();
+                }
+            });
         }
     ])
 });
