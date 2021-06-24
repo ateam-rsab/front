e9e0d384d49443d460e7750bda7cf253bad774a0
Instalasi Teknologi Informasi <Instalasi-TI@MacITI.local>
2021-06-24 08:50:25 +0700

    done export excel

--- app/module/SDM/LogbookKinerjaDokterCtrl.js
+++ app/module/SDM/LogbookKinerjaDokterCtrl.js
@@ -14,7 +14,7 @@ define(['initialize'], function (initialize) {
 
             let groupJSON = function (xs, key) {
                 return xs.reduce(function (rv, x) {
-                    
+
                     (rv[x[key]] = rv[x[key]] || []).push(x);
                     return rv;
                 }, {});
@@ -41,7 +41,8 @@ define(['initialize'], function (initialize) {
                 for (let i = 0; i < $scope.daysInMonth; i++) {
                     $scope.headerTable.push({
                         width: "20px",
-                        title: i + 1
+                        title: i + 1,
+                        value: i + 1
                     });
                 }
             }
@@ -57,14 +58,15 @@ define(['initialize'], function (initialize) {
             $scope.getDataLogbook = () => {
                 getHeaderTable();
 
-                if (!$scope.item.pegawai) {
-                    toastr.info("Harap pilih pegawai terlebih dahulu");
-                    return;
-                }
+                // if (!$scope.item.pegawai) {
+                //     toastr.info("Harap pilih pegawai terlebih dahulu");
+                //     return;
+                // }
                 $scope.isRouteLoading = true;
                 let dataTemp = [];
                 // ${$scope.item.periode ? dateHelper.toTimeStamp($scope.item.periode) : dateHelper.toTimeStamp(new Date())}
                 // ${$scope.item.pegawai.id}
+                // Request URL: http://192.168.12.3:8080/jasamedika-sdm/iki-remunerasi/get-logbook-skoring-dokter?bulan=1614127280000&pegawaiId=3
                 ManageSdmNew.getListData(`iki-remunerasi/get-logbook-skoring-dokter?bulan=${$scope.item.periode ? dateHelper.toTimeStamp($scope.item.periode) : dateHelper.toTimeStamp(new Date())}&pegawaiId=${$scope.item.pegawai.id}`).then(res => {
 
                     for (let i = 0; i < res.data.data.length; i++) {
@@ -104,29 +106,108 @@ define(['initialize'], function (initialize) {
                     let groupedJSON = groupJSON(dataTemp, 'namaIndikator');
                     let formattedJSON = Object.keys(groupedJSON).map((key) => [(key), groupedJSON[key]]);
                     let dataGet = [];
-                    
-                    for(let i = 0; i < formattedJSON.length; i++) {
+
+                    for (let i = 0; i < formattedJSON.length; i++) {
                         dataGet.push({
-                            label:formattedJSON[i][0],
+                            label: formattedJSON[i][0],
                             detail: [],
-                            subTotalSkor:0,
+                            subTotalSkor: 0,
                             subJumlah: 0,
                             subSkor: 0
                         })
 
-                        for(let ii = 0; ii < formattedJSON[i][1].length; ii++) {
+                        for (let ii = 0; ii < formattedJSON[i][1].length; ii++) {
                             dataGet[i].detail.push(formattedJSON[i][1][ii]);
                             dataGet[i].subJumlah += formattedJSON[i][1][ii].totalSkor;
                             dataGet[i].subSkor += formattedJSON[i][1][ii].skor;
                             dataGet[i].subTotalSkor += formattedJSON[i][1][ii].totalSkor;
                         }
                         $scope.grandTotal = dataGet[i].subTotalSkor + $scope.grandTotal;
-                    }   
+                    }
                     $scope.dataSource = dataGet;
+
+                    $scope.kendoDataSource = new kendo.data.DataSource({
+                        data: dataGet,
+                        pageSize: 100
+                    })
                     $scope.isRouteLoading = false;
-                })
+                });
             }
-            // $scope.getDataLogbook();
+            $scope.getDataLogbook();
+
+            $scope.exportExcel = () => {
+
+                var rows = [{
+                    cells: [{ value: "Nama Kegiatan" },
+                    { value: "Jumlah" },
+                    { value: "Skor" },
+                    { value: "Total Skor" },
+                    ...$scope.headerTable,
+                    ]
+                }];
+
+                let tempDataExport = $scope.kendoDataSource;
+                tempDataExport.fetch(() => {
+                    var data = tempDataExport._data;
+                    let dataTemp = [];
+                    
+                    for(let i = 0; i < data.length; i++) {
+                        dataTemp.push(data[i])
+                        for(let ii = 0; ii < data[i].detail.length; ii++) {
+                            dataTemp.push(data[i].detail[ii]);
+                        }
+                    }
+                    let dataTgl = []
+                    for (var i = 0; i < dataTemp.length; i++) {
+                        dataTgl = [];
+                        if(dataTemp[i].dataDetail) {
+                            for(let ii = 0; ii < dataTemp[i].dataDetail.length; ii++) {
+                                dataTgl.push({
+                                    value: dataTemp[i].dataDetail[ii].jmlLayanan
+                                })
+                            }
+                        }
+                       
+                        //push single row for every record
+                        rows.push({
+                            cells: [{
+                                value: dataTemp[i].label ? dataTemp[i].label : dataTemp[i].namaProduk
+                            },
+                            {
+                                value: dataTemp[i].jumlah ? dataTemp[i].jumlah : ""
+                            },
+                            {
+                                value: dataTemp[i].skor ? dataTemp[i].skor : ""
+                            },
+                            {
+                                value: dataTemp[i].totalSkor ? dataTemp[i].totalSkor : ""
+                            },
+                            ...dataTgl
+                          ]
+                        })
+                    }
+
+                    console.log(rows);
+                    var workbook = new kendo.ooxml.Workbook({
+                        mergedCells: ["A1:B4"],
+                        sheets: [{
+                            freezePane: {
+                                rowSplit: 1
+                            },
+                            columns: [],
+                            // Title of the sheet
+                            title: "Logbook Kinerja Dokter",
+                            // Rows of the sheet
+                            rows: rows
+                        }]
+                    });
+                    // save the file as Excel file with extension xlsx
+                    kendo.saveAs({
+                        dataURI: workbook.toDataURL(),
+                        fileName: "logbook-kinerja-dokter.xlsx"
+                    });
+                });
+            };
         }
     ])
 });
