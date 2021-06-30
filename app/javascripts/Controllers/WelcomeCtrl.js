94cba035a9cba598d6b05b5aa1ef9af3d342d1c7
salmanoe <salmanoe@live.com>
2021-06-30 16:51:43 +0700

    penyesuaian redaksional notifikasi masa berlaku sip dan str karena masa status bencana nasional covid-19

--- app/javascripts/Controllers/WelcomeCtrl.js
+++ app/javascripts/Controllers/WelcomeCtrl.js
@@ -21,31 +21,31 @@ define(['initialize'], function (initialize) {
             let getDataAbsensi = () => {
                 let today = DateHelper.formatDate(new Date(), "YYYY-MM-DD");
                 let dataPegawai = JSON.parse(localStorage.getItem("pegawai"));
-                
-                MenuService.getNotification('sdm/get-kehadiran/' + dataPegawai.id + '/' + today + '/' + today).then(res => { 
+
+                MenuService.getNotification('sdm/get-kehadiran/' + dataPegawai.id + '/' + today + '/' + today).then(res => {
                     let dataKehadiran = res.data.data.listkehadiran[0];
 
                     dataKehadiran.isAbsensiPulang = dataKehadiran.absensiPulang === '-' ? false : true;
                     dataKehadiran.isAbsensiMasuk = dataKehadiran.absensiMasuk === '-' ? false : true;
                     // console.log(dataKehadiran);
 
-                    if(dataKehadiran.namaShift === "Libur") {
+                    if (dataKehadiran.namaShift === "Libur") {
                         $scope.showNotif.messageAbsensi = false;
                         return;
                     }
 
-                    if(!dataKehadiran.isAbsensiMasuk) {
+                    if (!dataKehadiran.isAbsensiMasuk) {
                         $scope.showNotif.messageAbsensi = true;
                         $scope.messageAbsensi = "Anda belum absensi masuk hari ini";
                         return;
                     }
 
-                    if(!dataKehadiran.isAbsensiPulang) {
+                    if (!dataKehadiran.isAbsensiPulang) {
                         $scope.showNotif.messageAbsensi = true;
                         $scope.messageAbsensi = "Anda belum absensi pulang hari ini";
                         return;
                     }
-                    
+
                 });
             }
             getDataAbsensi();
@@ -66,49 +66,41 @@ define(['initialize'], function (initialize) {
 
                     if (data.tglBerakhirSip && data.tglBerakhirStr) {
                         if (data.tglBerakhirSip >= DateHelper.toTimeStamp($scope.now) && data.tglBerakhirStr >= DateHelper.toTimeStamp($scope.now)) {
-                            $scope.messageNotif = `SIP dan STR Anda akan berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirSip))} dan ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirStr))}. Mohon hubungi Bagian SDM untuk mengkonfirmasi status SIP dan STR terbaru Anda. Terima kasih`;
+                            $scope.messageNotif = `SIP dan STR Anda akan berakhir masing-masing pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirSip))} dan ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirStr))}. Dengan Surat Edaran No. HK.02.03/XXI.4/285/2021 tentang perpanjangan SIP/ STR dalam masa Status Bencana Nasional Covid-19, perpanjangan dapat dilakukan sampai 1 tahun setelah tanggal berakhir. Mohon hubungi Bagian SDM untuk mengkonfirmasi status SIP dan STR terbaru Anda. Terima kasih`;
 
-                            $scope. popUp.open().center()
-                            // $scope.showSIPExpired = true;
+                            $scope.popUp.open().center()
                         } else if (data.tglBerakhirSip < DateHelper.toTimeStamp($scope.now) && data.tglBerakhirStr >= DateHelper.toTimeStamp($scope.now)) {
-                            $scope.messageNotif = `Mohon hubungi Bagian SDM karena SIP Anda telah berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirSip))}. STR Anda akan berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirStr))}, mohon hubungi Bagian SDM untuk mengkonfirmasi status STR terbaru Anda. Terima kasih`;
+                            $scope.messageNotif = `SIP Anda telah berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirSip))}. STR Anda akan berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirStr))}. Dengan Surat Edaran No. HK.02.03/XXI.4/285/2021 tentang perpanjangan SIP/ STR dalam masa Status Bencana Nasional Covid-19, perpanjangan dapat dilakukan sampai 1 tahun setelah tanggal berakhir. Mohon hubungi Bagian SDM untuk mengkonfirmasi status SIP terbaru Anda. Terima kasih`;
 
-                            $scope. popUp.open().center()
-                            // $scope.showSIPExpired = true;
+                            $scope.popUp.open().center()
                         } else if (data.tglBerakhirSip >= DateHelper.toTimeStamp($scope.now) && data.tglBerakhirStr < DateHelper.toTimeStamp($scope.now)) {
-                            $scope.messageNotif = `Mohon hubungi Bagian SDM karena STR Anda telah berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirStr))}. SIP Anda akan berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirSip))}, mohon hubungi Bagian SDM untuk mengkonfirmasi status SIP terbaru Anda. Terima kasih`;
+                            $scope.messageNotif = `STR Anda telah berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirStr))}. SIP Anda akan berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirSip))}. Dengan Surat Edaran No. HK.02.03/XXI.4/285/2021 tentang perpanjangan SIP/ STR dalam masa Status Bencana Nasional Covid-19, perpanjangan dapat dilakukan sampai 1 tahun setelah tanggal berakhir. Mohon hubungi Bagian SDM untuk mengkonfirmasi status STR terbaru Anda. Terima kasih`;
 
-                            $scope. popUp.open().center()
-                            // $scope.showSIPExpired = true;
+                            $scope.popUp.open().center()
                         } else if (data.tglBerakhirSip < DateHelper.toTimeStamp($scope.now) && data.tglBerakhirStr < DateHelper.toTimeStamp($scope.now)) {
-                            $scope.messageNotif = `Mohon hubungi Bagian SDM karena SIP dan STR Anda telah berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirSip))} dan ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirStr))}. Terima kasih`;
+                            $scope.messageNotif = `SIP dan STR Anda telah berakhir masing-masing pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirSip))} dan ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirStr))}. Dengan Surat Edaran No. HK.02.03/XXI.4/285/2021 tentang perpanjangan SIP/ STR dalam masa Status Bencana Nasional Covid-19, perpanjangan dapat dilakukan sampai 1 tahun setelah tanggal berakhir. Mohon hubungi Bagian SDM untuk mengkonfirmasi status SIP dan STR terbaru Anda. Terima kasih`;
 
-                            $scope. popUp.open().center()
-                            // $scope.showSIPExpired = true;
+                            $scope.popUp.open().center()
                         }
                     } else if (data.tglBerakhirSip) {
                         if (data.tglBerakhirSip >= DateHelper.toTimeStamp($scope.now)) {
-                            $scope.messageNotif = `SIP Anda akan berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirSip))}. Mohon hubungi Bagian SDM untuk mengkonfirmasi status SIP terbaru Anda. Terima kasih`;
+                            $scope.messageNotif = `SIP Anda akan berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirSip))}. Dengan Surat Edaran No. HK.02.03/XXI.4/285/2021 tentang perpanjangan SIP/ STR dalam masa Status Bencana Nasional Covid-19, perpanjangan dapat dilakukan sampai 1 tahun setelah tanggal berakhir. Mohon hubungi Bagian SDM untuk mengkonfirmasi status SIP terbaru Anda. Terima kasih`;
 
-                            $scope. popUp.open().center()
-                            // $scope.showSIPExpired = true;
+                            $scope.popUp.open().center()
                         } else if (data.tglBerakhirSip < DateHelper.toTimeStamp($scope.now)) {
-                            $scope.messageNotif = `Mohon hubungi Bagian SDM karena SIP Anda telah berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirSip))}. Terima kasih`;
+                            $scope.messageNotif = `SIP Anda telah berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirSip))}. Dengan Surat Edaran No. HK.02.03/XXI.4/285/2021 tentang perpanjangan SIP/ STR dalam masa Status Bencana Nasional Covid-19, perpanjangan dapat dilakukan sampai 1 tahun setelah tanggal berakhir. Mohon hubungi Bagian SDM untuk mengkonfirmasi status SIP terbaru Anda. Terima kasih`;
 
-                            $scope. popUp.open().center()
-                            // $scope.showSIPExpired = true;
+                            $scope.popUp.open().center()
                         }
                     } else if (data.tglBerakhirStr) {
                         if (data.tglBerakhirStr >= DateHelper.toTimeStamp($scope.now)) {
-                            $scope.messageNotif = `STR Anda akan berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirStr))}. Mohon hubungi Bagian SDM untuk mengkonfirmasi status STR terbaru Anda. Terima kasih`;
+                            $scope.messageNotif = `STR Anda akan berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirStr))}. Dengan Surat Edaran No. HK.02.03/XXI.4/285/2021 tentang perpanjangan SIP/ STR dalam masa Status Bencana Nasional Covid-19, perpanjangan dapat dilakukan sampai 1 tahun setelah tanggal berakhir. Mohon hubungi Bagian SDM untuk mengkonfirmasi status STR terbaru Anda. Terima kasih`;
 
-                            $scope. popUp.open().center()
-                            // $scope.showSIPExpired = true;
+                            $scope.popUp.open().center()
                         } else if (data.tglBerakhirStr < DateHelper.toTimeStamp($scope.now)) {
-                            $scope.messageNotif = `Mohon hubungi Bagian SDM karena STR Anda telah berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirStr))}. Terima kasih`;
+                            $scope.messageNotif = `STR Anda telah berakhir pada ${DateHelper.getTanggalFormatted(new Date(data.tglBerakhirStr))}. Dengan Surat Edaran No. HK.02.03/XXI.4/285/2021 tentang perpanjangan SIP/ STR dalam masa Status Bencana Nasional Covid-19, perpanjangan dapat dilakukan sampai 1 tahun setelah tanggal berakhir. Mohon hubungi Bagian SDM untuk mengkonfirmasi status STR terbaru Anda. Terima kasih`;
 
-                            $scope. popUp.open().center()
-                            // $scope.showSIPExpired = true;
+                            $scope.popUp.open().center()
                         }
                     }
                 });
