f758f0bf0852da58616092350e0a5cd58cbf0e84
salmanoe <salmanoe@live.com>
2021-08-12 13:49:03 +0700

    - pemasangan pilihan pegawai atau keluarga inti saat diskon di tata rekening - perbaikan service hitung dan simpan diskon di tata rekening

--- app/module/Akuntansi/TataRekening/VerifikasiTagihanCtrl.js
+++ app/module/Akuntansi/TataRekening/VerifikasiTagihanCtrl.js
@@ -10,42 +10,44 @@ define(['initialize'], function (initialize) {
 			$scope.showKelengkapanDokumen = false;
 
 			$scope.item = {};
+			$scope.item.diskonpegawai = 0;
 			$scope.tombolSaveIlang = true;
 			$scope.isDiskonRSAB = false;
 			$scope.isDiskonKaryawanKeluargaInti = false;
 
 			$scope.dataParams = JSON.parse($state.params.dataPasien);
 
-			$q.all([
-				modelItemAkuntansi.getDataTableTransaksi("tatarekening/verifikasi-tagihan2/" + $scope.dataParams.noRegistrasi)
-			])
-				.then(function (data) {
+			// $q.all([
+			// 	modelItemAkuntansi.getDataTableTransaksi("tatarekening/verifikasi-tagihan2/"
+			// 		+ $scope.dataParams.noRegistrasi + "?jenisdiskon=" + $scope.item.diskonpegawai)
+			// ])
+			// 	.then(function (data) {
 
-					if (data[0].statResponse) {
-						$scope.isDiskonRSAB = data[0].kelompokPasienID === 1;
+			// 		if (data[0].statResponse) {
+			// 			$scope.isDiskonRSAB = data[0].kelompokPasienID === 1;
 
-						$scope.item = data[0];
-						if (data[0].needDokument) {
-							$scope.showKelengkapanDokumen = true;
-							$scope.dataKelengkapanDokumen = new kendo.data.DataSource({
-								data: data[0].dokuments
-							});
-						}
-						if ($scope.item.jenisPasien == 'BPJS') {
-							// $scope.item.totalKlaim = $scope.item.jumlahBayar //Old
-							$scope.item.totalKlaim = $scope.item.jumlahBayarNew //New
-						}
-						$scope.showTtlKlaim = true
-						$scope.showTtlKlaim2 = false
-						if (data[0].isRawatInap == false) {
-							if (data[0].jenisPasien == 'Umum/Pribadi') {
-								$scope.showTtlKlaim = false
-								$scope.showTtlKlaim2 = true
-							}
-						}
-					}
-					$scope.isRouteLoading = false
-				});
+			// 			$scope.item = data[0];
+			// 			if (data[0].needDokument) {
+			// 				$scope.showKelengkapanDokumen = true;
+			// 				$scope.dataKelengkapanDokumen = new kendo.data.DataSource({
+			// 					data: data[0].dokuments
+			// 				});
+			// 			}
+			// 			if ($scope.item.jenisPasien == 'BPJS') {
+			// 				// $scope.item.totalKlaim = $scope.item.jumlahBayar //Old
+			// 				$scope.item.totalKlaim = $scope.item.jumlahBayarNew //New
+			// 			}
+			// 			$scope.showTtlKlaim = true
+			// 			$scope.showTtlKlaim2 = false
+			// 			if (data[0].isRawatInap == false) {
+			// 				if (data[0].jenisPasien == 'Umum/Pribadi') {
+			// 					$scope.showTtlKlaim = false
+			// 					$scope.showTtlKlaim2 = true
+			// 				}
+			// 			}
+			// 		}
+			// 		$scope.isRouteLoading = false
+			// 	});
 
 			// function showButton(){
 			// 	$scope.showBtnSimpan = true;
@@ -53,6 +55,41 @@ define(['initialize'], function (initialize) {
 			// 	// $scope.showBtnCetak = true;
 			// }
 			// showButton();
+
+			$scope.loadDataVerif()
+
+			$scope.loadDataVerif = function () {
+				$scope.isRouteLoading = true
+
+				modelItemAkuntansi.getDataTableTransaksi("tatarekening/verifikasi-tagihan2/"
+					+ $scope.dataParams.noRegistrasi + "?jenisdiskon=" + $scope.item.diskonpegawai).then(function (data) {
+						if (data.statResponse) {
+							$scope.isDiskonRSAB = data.kelompokPasienID === 1;
+
+							$scope.item = data;
+							if (data.needDokument) {
+								$scope.showKelengkapanDokumen = true;
+								$scope.dataKelengkapanDokumen = new kendo.data.DataSource({
+									data: data.dokuments
+								});
+							}
+							if ($scope.item.jenisPasien == 'BPJS') {
+								// $scope.item.totalKlaim = $scope.item.jumlahBayar //Old
+								$scope.item.totalKlaim = $scope.item.jumlahBayarNew //New
+							}
+							$scope.showTtlKlaim = true
+							$scope.showTtlKlaim2 = false
+							if ($scope.item.diskonpegawai == 1 || $scope.item.diskonpegawai == 2) {
+								if (data.jenisPasien == 'Umum/Pribadi') {
+									$scope.showTtlKlaim = false
+									$scope.showTtlKlaim2 = true
+									$scope.item.totalKlaim = data.totalDiskonPegawai
+								}
+							}
+						}
+						$scope.isRouteLoading = false
+					})
+			}
 
 			$scope.Cetak = function () {
 				$scope.pageCetak = true;
@@ -83,44 +120,47 @@ define(['initialize'], function (initialize) {
 
 				if (isValid.status) {
 					if ($scope.isDiskonRSAB && $scope.isDiskonKaryawanKeluargaInti) {
-						if ($scope.item.totalKlaim > $scope.item.billing / 2) {
+						// if ($scope.item.totalKlaim > $scope.item.billing / 2) {
+						// 	$scope.isRouteLoading = false
+						// 	$scope.tombolSaveIlang = true
+						// 	toastr.warning("Total diskon karyawan/ keluarga inti tidak boleh lebih besar dari 50% Total Billing!")
+						// 	return
+						// } else {
+
+						$scope.isRouteLoading = false
+						var confirm = $mdDialog.confirm()
+							.title('Konfirmasi Diskon Karyawan/ Keluarga Inti')
+							.textContent('Diskon karyawan/ keluarga inti sebesar ' + formatRupiah($scope.item.totalKlaim, "Rp"))
+							.ariaLabel('Lucky day')
+							.targetEvent(e)
+							.ok('OK')
+							.cancel('Cancel');
+						$mdDialog.show(confirm).then(function () {
+							$scope.isRouteLoading = true
+							manageSdmNew.getListData("pelayanan/klaim-diskon-karyawan?noRegistrasi=" + $scope.item.noRegistrasi
+								+ "&totalKlaim=" + $scope.item.totalKlaim + "&jenisDiskon=" + $scope.item.diskonpegawai)
+								.then(function (e) {
+									//set nol kembali karena klaim ke diri sendiri bukan ke penjamin/ pihak ketiga
+									$scope.item.totalKlaim = 0;
+									$scope.item.isDiskon = $scope.item.diskonpegawai
+									manageTataRekening.saveVerifikasiTagihan($scope.item)
+										.then(function (e) {
+											$scope.SaveLogUser();
+											window.history.back();
+											$scope.isRouteLoading = false
+											$scope.tombolSaveIlang = true;
+										}, function () {
+											$scope.isRouteLoading = false
+											$scope.tombolSaveIlang = true;
+										});
+								});
+						}, function () {
+							// error function
 							$scope.isRouteLoading = false
 							$scope.tombolSaveIlang = true
-							toastr.warning("Total diskon karyawan/ keluarga inti tidak boleh lebih besar dari 50% Total Billing!")
-							return
-						} else {
-							$scope.isRouteLoading = false
-							var confirm = $mdDialog.confirm()
-								.title('Konfirmasi Diskon Karyawan/ Keluarga Inti')
-								.textContent('Diskon karyawan/ keluarga inti sebesar ' + formatRupiah($scope.item.totalKlaim, "Rp"))
-								.ariaLabel('Lucky day')
-								.targetEvent(e)
-								.ok('OK')
-								.cancel('Cancel');
-							$mdDialog.show(confirm).then(function () {
-								$scope.isRouteLoading = true
-								manageSdmNew.getListData("pelayanan/klaim-diskon-karyawan?noRegistrasi=" + $scope.item.noRegistrasi + "&totalKlaim=" + $scope.item.totalKlaim)
-									.then(function (e) {
-										//set nol kembali karena klaim ke diri sendiri bukan ke penjamin/ pihak ketiga
-										$scope.item.totalKlaim = 0;
-										$scope.item.isDiskon = $scope.isDiskonKaryawanKeluargaInti
-										manageTataRekening.saveVerifikasiTagihan($scope.item)
-											.then(function (e) {
-												$scope.SaveLogUser();
-												window.history.back();
-												$scope.isRouteLoading = false
-												$scope.tombolSaveIlang = true;
-											}, function () {
-												$scope.isRouteLoading = false
-												$scope.tombolSaveIlang = true;
-											});
-									});
-							}, function () {
-								// error function
-								$scope.isRouteLoading = false
-								$scope.tombolSaveIlang = true
-							});
-						}
+						});
+
+						// }
 					} else {
 						manageTataRekening.saveVerifikasiTagihan($scope.item)
 							.then(function (e) {
@@ -200,10 +240,13 @@ define(['initialize'], function (initialize) {
 				if (inputId.indexOf("diskon") >= 0 && checked) {
 					$scope.showTtlKlaim = true
 					$scope.showTtlKlaim2 = false
+					$scope.isAsPegOrKel = true
 				} else {
 					$scope.showTtlKlaim = false
 					$scope.showTtlKlaim2 = true
 					$scope.item.totalKlaim = 0
+					$scope.isAsPegOrKel = false
+					$scope.item.diskonpegawai = 0
 				}
 			};
 		}
