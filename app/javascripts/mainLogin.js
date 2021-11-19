3ca2acdb6dcbb625dba80e515e7b0e1c0ee65dec
salmanoe <salmanoe@live.com>
2021-11-19 08:55:09 +0700

    perbaikan label set item localstorage untuk koordinat sotk

--- app/javascripts/mainLogin.js
+++ app/javascripts/mainLogin.js
@@ -124,7 +124,7 @@ require(['LoginService', 'core', "kendo.angular", 'Configuration', 'Helper', 'jQ
 
                                 window.localStorage.setItem('datauserlogin', JSON.stringify(dataUserLogin));
                                 window.localStorage.setItem('pegawai', JSON.stringify(data.data.data.pegawai));
-                                window.localStorage.setItem('mapJabatanUnitKerjaToPegawai', JSON.stringify(data.data.data.mapJabatanUnitKerjaToPegawai));
+                                window.localStorage.setItem('sotk_coor', JSON.stringify(data.data.data.sotk_coor));
 
                                 // ${'dataLogin'.replace(/[a-zA-Z0-9]/g, m => keyEnc[m])}
                                 // document.cookie = `dataLogin=${JSON.stringify({namaUser: userName,kataSandi: password}).replace(/[a-zA-Z0-9]/g, m => keyEnc[m])};`;
